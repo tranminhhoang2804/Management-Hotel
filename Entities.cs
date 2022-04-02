baaa66af81973c1044c93d91d46ad8adaf849f5b
tranminhhoang2804 <hoangvip9x2001@gmail.com>
2022-04-02 21:00:36 +0700

    first commit

---
+++ Entities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    [Serializable]
+    public partial class Entities
+    {
+        private Entities(DbConnection connectionString, bool contextOwnsConnection = true):base(connectionString, contextOwnsConnection) { }
+        public static Entities CreateEntities(bool contextOwnsConnection = true)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.Open("connectdb.data", FileMode.Open, FileAccess.Read);
+            connect cp = (connect)bf.Deserialize(fs);
+
+            string servername = Encryptor.Decrypt(cp.servername, "qwertyuiop", true);
+            string username = Encryptor.Decrypt(cp.username, "qwertyuiop", true);
+            string pass = Encryptor.Decrypt(cp.passwd, "qwertyuiop", true);
+            string database = Encryptor.Decrypt(cp.database, "qwertyuiop", true);
+
+            SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder();
+            SqlConnectionStringBuilder sqlConnectBuilder = new SqlConnectionStringBuilder();
+            sqlConnectBuilder.DataSource = servername;
+            sqlConnectBuilder.InitialCatalog = database;
+            sqlConnectBuilder.UserID = username;
+            sqlConnectBuilder.Password = pass;
+
+            string sqlConnectionString = sqlConnectBuilder.ConnectionString;
+
+            EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder();
+            entityBuilder.Provider = "System.Data.SqlClient";
+            entityBuilder.ProviderConnectionString = sqlConnectionString;
+
+            entityBuilder.Metadata = @"res://*/KHACHSAN.csdl|res://*/KHACHSAN.ssdl|res://*/KHACHSAN.msl";
+
+            EntityConnection connection = new EntityConnection(entityBuilder.ConnectionString);
+
+            fs.Close();
+            return new Entities(connection);
+
+        }
+    }
+}
